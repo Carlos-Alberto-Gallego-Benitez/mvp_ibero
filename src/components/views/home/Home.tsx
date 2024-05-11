2e53a615881bc2e3a33677d6e488a9843ddecf6a
CARLOS\Acer <carlosalgabez.08@gmail.com>
2024-05-11 18:25:54 -0500

    ultimate mvp

--- src/components/views/home/Home.tsx
+++ src/components/views/home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { ChangeEvent, Fragment } from "react";
 import neongomez1 from "../../../images/carrusel/neongomez1.png";
 // import logoIBero from "../../../images/logos/1-Logo-IBEROAMERICANA.png"
 import lofoFulldev from "../../../images/logos/FullDev_Mesadetrabajo.png";
@@ -9,9 +9,49 @@ import iconoNen from "../../../icons/neon.png"
 import nuketowsoft from "../../../icons/nuketowsoft.png"
 import videoneongomez from "../../../videos/videoneongomez.mp4"
 import segGit from "../../../images/general/seggit.png"
+import commits from "../../../images/general/commits.png"
+import tablero from "../../../images/general/tablero.png"
+import { Chart } from "react-google-charts";
 
-const Home = () : React.ReactElement => {
-    
+
+
+
+
+const Home = (): React.ReactElement => {
+
+    function seletect(e : ChangeEvent<HTMLSelectElement>) {
+        if (e.target.value == '1') {
+            window.open('https://carlosalgabez08.atlassian.net/jira/software/projects/KAN/boards/1?atlOrigin=eyJpIjoiMzYxMjY2MGFhNzIyNDkyMmE5ZTAzMGYxMDE2MmI3MDciLCJwIjoiaiJ9');  
+        }
+        if (e.target.value == '2') {
+            window.open('https://carlosalgabez08.atlassian.net/jira/software/projects/KAN/boards/1/timeline?shared=&atlOrigin=eyJpIjoiNGM5ZjVlZjk1ODBkNDYyMzk0YTEyYzM3YjdkMjViOWYiLCJwIjoiaiJ9');  
+        }
+        if (e.target.value == '3') {
+            window.open('https://carlosalgabez08.atlassian.net/wiki/external/ZGI3Nzc1MDgwZDYzNDE0YzhlYzAyNjg4MDBmYWJmNjg');  
+        }
+    }
+
+     const data = [
+        ["Gestión", "Scrum", "Kanban", "Agilismo", "Desarrollo", "Integración"],
+        ["2020", 1526000, 2526000, 1526000,5008000, 6008000],
+        ["2021", 3792000, 3792000,  3792000, 7500000, 5008000],
+        ["2022", 5695000, 5895000,  5995000, 7808000, 8008000],
+        ["2023", 7695000, 7795000,  7995000, 8808000, 9008000],
+        ["2024", 9999999, 9999999,  9999999, 9999999, 9999999],
+        ];
+
+     const options = {
+        title: "Scrum dentro de Neón Gómez impacto en Nuketow Soft",
+        chartArea: { width: "50%" },
+        hAxis: {
+            title: "Escala de valorización",
+            minValue: 0,
+        },
+        vAxis: {
+            title: "Tiempo",
+        },
+    };
+
     return (
         // <img alt="" height={'30'}  src={logoIBero} />
         <Fragment>
@@ -63,7 +103,6 @@ const Home = () : React.ReactElement => {
                              <img src={perfilneon} alt="image 1" />
                         </label>
                     </div>
-                    <div id="particles-js" className="absolute z-10 inset-0"><canvas className="particles-js-canvas-el margenesparticle"  width="1357" height="607"></canvas></div>
                 </div>
 
                 <div className="banner-overlay"></div>
@@ -71,35 +110,24 @@ const Home = () : React.ReactElement => {
             </section>
 
             <main>
-                <article className="container">
-                    <div className="row align-items-center">
-                        <div className="col-md-4">
-
-                            <p className="mb-4">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem dignissimos sit ipsam
-                                officia eaque, ut eveniet fugit laboriosam commodi reprehenderit quos sunt tempore, temporibus
-                                dolore cupiditate veniam totam! Iure, sequi.
-                            </p>
-                        </div>
-                        <div className="col-md-4">
-                            <img src="./imagenes/Image5.jpg" className="rounded mx-auto d-block" alt="Imgage 5" />
-                        </div>
-                        <div className="col-md-4">
-                            <p className="mb-4">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus deserunt sint, ducimus
-                                facere illo accusantium quos iste minus architecto asperiores voluptatem totam at impedit labore
-                                incidunt explicabo? Excepturi, alias rem.
-                            </p>
-                        </div>
-                    </div>
-                </article>
+     
                 
-                <article className="container mt-5">
+                <article className="container-repl mt-5">
                     <div className="row justify-content-center section-text">
                         <div className="col-md-6 align-self-center">
                             <p className="mb-8">
                                 Neón Gómez  es una empresa de venta de anuncios publicitarios hechos en acrílico y neón, a lo largo de su trayectoria empresarial se ha enfrentado a grandes retos en el mercado y afortunadamente se ha expandido de manera extraordinaria es por eso que se ha sistematizado toda la gestión de venas compra y fabricación de anuncios mediante el sistema  Neón Gómez – Nuketow Soft para cubrir la necesidad de sistematizar toda la información de Neón Gómez en Nuketow Soft permitiendo así obtener detalles claros de recursos  e información en tiempo real de todo lo que sucede en Neón Gómez 
                             </p>
+                            
+                        </div>
+                        <div>
+                            <Chart
+                            chartType="BarChart"
+                            width="100%"
+                            height="400px"
+                            data={data}
+                            options={options}
+                            />
                         </div>
                         <div className="centen-imglogo">
                             <img className="icononeon" src={iconoNen} alt="" />
@@ -108,12 +136,12 @@ const Home = () : React.ReactElement => {
                     </div>
                 </article>
 
-                <article className="container">
+                <article className="container-repl">
                     <div className="row align-items-center">
                         <div className="col-md-4">
 
                             <p className="mb-4">
-                                A lo largo del desarrollo se  pudo implementar el desarrollo de metodologías agiles como scrum mediante este proyecto, el cual  generado bajo la cobertura de 7 sprint de desarrollo con una duración de 11 días cada sprint aplicando seguimientos como dailys, retrospectivas,  estimaciones de tiempo y requerimientos por lo que fue necesario la ayuda de un equipo conformado por QA, Lider, Desarrolladores y Arquitectos, para poder completar con éxito esta labor, se implementó también el seguimiento al código mediante git y github
+                                A lo largo del desarrollo se  pudo implementar el uso de metodologías agiles como <strong>Scrum</strong> mediante este proyecto, el cual  generado bajo la cobertura de 7 sprint de desarrollo con una duración de 11 días cada sprint aplicando seguimientos como dailys, retrospectivas,  estimaciones de tiempo y requerimientos por lo que fue necesario la ayuda de un equipo conformado por QA, Lider, Desarrolladores y Arquitectos, para poder completar con éxito esta labor, se implementó también el seguimiento al código mediante git y github
                             </p>
                         </div>
                         <div className="col-md-4">
@@ -127,61 +155,48 @@ const Home = () : React.ReactElement => {
                     </div>
                 </article>
 
-                <article className="container">
+                
+                <article className="container-repl">
                     <div className="row align-items-center">
-                        <div className="col-md-4">
-                            <div className="drawer">
-                                <select>
-                                    <option value="opcion1">Opción 1</option>
-                                    <option value="opcion2">Opción 2</option>
-                                    <option value="opcion3">Opción 3</option>
-                                </select>
-                            </div>
+                        <div id=''>
+                            <img src={commits} alt="" />
                         </div>
-                        <div className="col-md-4">
-                            <p className="mb-4">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ut ad voluptates earum! Ducimus
-                                quaerat laborum odit, ab incidunt deserunt earum id, laboriosam voluptas ut quas optio suscipit
-                                corporis ratione.
-                            </p>
-                        </div>
-                        <div className="col-md-4">
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>columna 1</th>
-                                        <th>columna 2</th>
-                                        <th>columna 3</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Dato 1</td>
-                                        <td>Dato 2</td>
-                                        <td>Dato 3</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Dato 4</td>
-                                        <td>Dato 5</td>
-                                        <td>Dato 6</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Dato 7</td>
-                                        <td>Dato 8</td>
-                                        <td>Dato 9</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                        <div className="flexbox flexcenter">
+                            <a className="flexbox" href="https://github.com/Carlos-Alberto-Gallego-Benitez/NeonGomez/commits" target="_blank" rel="noopener noreferrer">
+                                <i className="icon-github"></i>
+                            </a>
                         </div>
                     </div>
                 </article>
-                <article className="container">
+
+                <article className="container-repl">
                     <div className="row align-items-center">
                         <div className="col-md-6">
-                            <p className="mb-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi dolorum, delectus
-                                eos natus laudantium recusandae explicabo? Nam velit voluptatem, qui neque, alias ab
-                                exercitationem quas beatae corporis porro, laboriosam hic.</p>
-                            <p className="mb-4">hol!</p>
+                            <label htmlFor="">Kanban Neón Gómez</label>
+                            <div className="drawer">
+                                <select onChange={(e) => { seletect(e) }}>
+                                    <option value="selce" selected={true}>Seleccione....</option>
+                                    <option value="1">Tablero Kanban</option>
+                                    <option value="2">Cronograma</option>
+                                    <option value={'3'}>Confluence</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <img src={tablero} alt="" />
+                        </div>
+                    </div>
+                </article>
+                <article className="container-repl">
+                    <div className="row align-items-center contentiframe">
+                        <iframe id='iframertask' className="iframeancho" width="100%" height="100%" src="https://kamban-tablero.netlify.app/"></iframe>
+                    </div>
+                </article>
+                <article className="container-repl">
+                    <div className="row align-items-center">
+                        <div className="col-md-6">
+                            <p className="mb-4">En el siguiente video se muestra el funcionamiento actual del desarrollo que se lleva, donde se ve la rapidez y rigidez del sistema Neón Gómez para procesar cada solitud  lo que indica una vez mas el correcto funcionamiento y cumplimiento del desarrollo</p>
+                            
                         </div>
                         <div className="col-md-6">
                             <video  className="img-fluid" autoPlay loop muted>
@@ -190,18 +205,18 @@ const Home = () : React.ReactElement => {
                         </div>
                     </div>
                 </article>
-                <article className="container">
-                    <form id="forExample" >
+                <article className="container-repl">
+                    <form id="forExample" action="#" >
                         <div className="row title-form">
-                            <label htmlFor="titleForm">Form</label>
+                            <label htmlFor="titleForm">Comunícanos </label>
                         </div>
                         <div className="row">
                             <div className="form-group col">
-                                <label htmlFor="inputEmail4">Primer Nombre</label>
+                                <label htmlFor="inputEmail4">Nombres</label>
                                 <input type="text" className="form-control" placeholder="Primer Nombre" />
                             </div>
                             <div className="form-group col">
-                                <label htmlFor="inputEmail4">Segundo Nombre</label>
+                                <label htmlFor="inputEmail4">Epellidos</label>
                                 <input type="text" className="form-control" placeholder="Segundo Nombre" />
                             </div>
                         </div>
@@ -216,7 +231,7 @@ const Home = () : React.ReactElement => {
                             </div>
                         </div>
                         <div className="row-izquierda">
-                            <button className="btn btn-primary">Enviar</button>
+                            <button className="btn btn-primary colorbtn">Enviar</button>
                         </div>
                     </form>
                 </article>
@@ -227,9 +242,10 @@ const Home = () : React.ReactElement => {
                     <div className="row">
 
                         <div className="col-md-4 culum">
-                            <h3>Items</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            <h3>Neón Gómez</h3>
+                            <p>Reparación y Creación de anuncios en Acrílico y Neón.</p>
+                            <img className="icononeon" src={iconoNen} alt="" />
+                            
                         </div>
 
                         <div className="col-md-4 Networks culum">
@@ -251,9 +267,9 @@ const Home = () : React.ReactElement => {
                         </div>
 
                         <div className="col-md-4 culum">
-                            <h3>Items</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            <h3>Nuketow Soft</h3>
+                            <p>Plataforma de gestión de ventas</p>
+                            <img className="icononuketowsoft" src={nuketowsoft} alt="" />
                         </div>
                     </div>
                 </div>
